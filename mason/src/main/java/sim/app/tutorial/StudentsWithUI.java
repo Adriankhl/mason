dca068fa3aafbc362e827a7fb5d60496985c00af
laikh <dlshcbmuipmam@hotmail.com>
2020-08-26 11:56:11 +0800

    Update tutorial

--- mason/src/main/java/sim/app/tutorial/StudentsWithUI.java
+++ mason/src/main/java/sim/app/tutorial/StudentsWithUI.java
@@ -1,14 +1,24 @@
 package sim.app.tutorial;
 
-import sim.portrayal.network.*;
-import sim.portrayal.continuous.*;
-import sim.engine.*;
-import sim.display.*;
-import sim.portrayal.simple.*;
-import sim.portrayal.*;
-import javax.swing.*;
+import sim.display.Console;
+import sim.display.Controller;
+import sim.display.Display2D;
+import sim.display.GUIState;
+import sim.engine.SimState;
+import sim.portrayal.DrawInfo2D;
+import sim.portrayal.Inspector;
+import sim.portrayal.continuous.ContinuousPortrayal2D;
+import sim.portrayal.network.NetworkPortrayal2D;
+import sim.portrayal.network.SimpleEdgePortrayal2D;
+import sim.portrayal.network.SpatialNetwork2D;
+import sim.portrayal.simple.CircledPortrayal2D;
+import sim.portrayal.simple.LabelledPortrayal2D;
+import sim.portrayal.simple.MovablePortrayal2D;
+import sim.portrayal.simple.OvalPortrayal2D;
+
+import javax.swing.JFrame;
 import java.awt.Color;
-import java.awt.*;
+import java.awt.Graphics2D;
 
 public class StudentsWithUI extends GUIState {
 
@@ -17,38 +27,44 @@ public class StudentsWithUI extends GUIState {
     ContinuousPortrayal2D yardPortrayal = new ContinuousPortrayal2D();
     NetworkPortrayal2D buddiesPortrayal = new NetworkPortrayal2D();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         StudentsWithUI vid = new StudentsWithUI();
         Console c = new Console(vid);
         c.setVisible(true);
     }
-    public StudentsWithUI() { super(new Students(System.currentTimeMillis())); }
-    public StudentsWithUI(SimState state) { super(state); }
 
-    public static String getName() { return "Student Schoolyard Cliques"; }
+    public StudentsWithUI() {
+        super(new Students(System.currentTimeMillis()));
+    }
+
+    public StudentsWithUI(SimState state) {
+        super(state);
+    }
+
+    public static String getName() {
+        return "Student Schoolyard Cliques";
+    }
 
     @Override
-    public Object getSimulationInspectedObject() { return state; }
+    public Object getSimulationInspectedObject() {
+        return state;
+    }
 
     @Override
-    public Inspector getInspector()
-    {
+    public Inspector getInspector() {
         Inspector i = super.getInspector();
         i.setVolatile(true);
         return i;
     }
 
     @Override
-    public void start()
-    {
+    public void start() {
         super.start();
         setupPortrayals();
     }
 
     @Override
-    public void load(SimState state)
-    {
+    public void load(SimState state) {
         super.load(state);
         setupPortrayals();
     }
@@ -57,17 +73,24 @@ public class StudentsWithUI extends GUIState {
         Students students = (Students) state;
         // tell the portrayals what to portray and how to portray them
         yardPortrayal.setField(students.yard);
-        yardPortrayal.setPortrayalForAll(new OvalPortrayal2D() {
-            public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
-                Student student = (Student) object;
-                int agitationShade = (int) (student.getAgitation() * 255 / 10.0);
-                if (agitationShade > 255) agitationShade = 255;
-                paint = new Color(agitationShade, 0, 255 - agitationShade);
-                super.draw(object, graphics, info);
-            }
-        });
+        yardPortrayal.setPortrayalForAll(
+                new MovablePortrayal2D(
+                        new CircledPortrayal2D(
+                                new LabelledPortrayal2D(
+                                        new OvalPortrayal2D() {
+                                            @Override
+                                            public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
+                                                Student student = (Student) object;
+                                                int agitationShade = (int) (student.getAgitation() * 255 / 10.0);
+                                                if (agitationShade > 255) agitationShade = 255;
+                                                paint = new Color(agitationShade, 0, 255 - agitationShade);
+                                                super.draw(object, graphics, info);
+                                            }
+                                        },
+                                        5.0, null, Color.black, true),
+                                0, 5.0, Color.green, true)));
 
-        buddiesPortrayal.setField( new SpatialNetwork2D( students.yard, students.buddies ) );
+        buddiesPortrayal.setField(new SpatialNetwork2D(students.yard, students.buddies));
         buddiesPortrayal.setPortrayalForAll(new SimpleEdgePortrayal2D());
 
         // reschedule the displayer
@@ -78,22 +101,20 @@ public class StudentsWithUI extends GUIState {
     }
 
     @Override
-    public void init(Controller c)
-    {
+    public void init(Controller c) {
         super.init(c);
-        display = new Display2D(600,600,this);
+        display = new Display2D(600, 600, this);
         display.setClipping(false);
         displayFrame = display.createFrame();
         displayFrame.setTitle("Schoolyard Display");
         c.registerFrame(displayFrame); // so the frame appears in the "Display" list
         displayFrame.setVisible(true);
-        display.attach( buddiesPortrayal, "Buddies" );
-        display.attach( yardPortrayal, "Yard" );
+        display.attach(buddiesPortrayal, "Buddies");
+        display.attach(yardPortrayal, "Yard");
     }
 
     @Override
-    public void quit()
-    {
+    public void quit() {
         super.quit();
         if (displayFrame != null) displayFrame.dispose();
         displayFrame = null;
